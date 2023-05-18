20b17f09b9d2f761b3dedd0863e27c57a950cd02
Cyrus Guest <cyrus.t.guest@gmail.com>
2023-05-18 11:04:32 -0400

    switch to dynamo db

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
 const express = require("express");
-const { Client } = require("pg");
 const cors = require("cors");
+const AWS = require("aws-sdk");
 const app = express();
-const client = new Client();
-
-client.connect();
 
 app.use(cors());
 app.use(express.json());
+
+AWS.config.update({ region: "us-east-1" });
+
+const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
 app.get("/api/v1/health", async (req, res) => {
   res.sendStatus(200);
@@ -17,18 +18,23 @@ app.get("/api/v1/health", async (req, res) => {
 app.post("/api/v1/create-message", async (req, res) => {
   const message = req.body;
 
-  const text =
-    "insert into messages(Email, Subject, Content) values($1, $2, $3) returning *";
-  const values = [message.email, message.subject, message.content];
+  const params = {
+    TableName: "bnb-message",
+    Item: {
+      MESSAGE_ID: { N: toString(Date.now()) },
+      MESSAGE_EMAIL: { S: message.email },
+      MESSAGE_SUBJECT: { S: message.subject },
+      MESSAGE_CONTENT: { S: message.content },
+    },
+  };
 
-  try {
-    const dbRes = await client.query(text, values);
-    res.status(200).json({ returnedMessage: dbRes.rows[0] });
-    console.log(res.rows[0]);
-  } catch (err) {
-    console.log(err.stack);
-    res.status(400).json({ returnedMessage: err });
-  }
+  ddb.putItem(params, (err, data) => {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
 });
 
 app.listen(5000, () => console.log("Running on port 5000"));
